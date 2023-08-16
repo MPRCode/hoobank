b1383aa8620ced007d7e13713e28c6f69b8a0cbb
MPRCode <113846964+MPRCode@users.noreply.github.com>
2023-08-16 17:06:20 +0100

    Initial Commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+const Button = ({ styles }) => {
+  return (
+    <button
+      type='button'
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary outline-none rounded-xl mt-5`}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
